a303504eaa89d86f4f8355eab15d7448e554b991
mehdi <0r0.developer@gmail.com>
2024-01-11 02:40:35 +0330

    add all needed apis

--- Mc2.CrudTest.Presentation/Server/Controllers/CustomerManagerController.cs
+++ Mc2.CrudTest.Presentation/Server/Controllers/CustomerManagerController.cs
@@ -1,10 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mc2.Application.Contracts.CustomerManagers;
+using Mc2.CrudTest.Presentation.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mc2.CrudTest.Presentation.Server.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CustomerManagerController :ControllerBase
+public class CustomerManagerController : ControllerBase
 {
-    
+    private readonly ICommandBus _commandBus;
+
+    public CustomerManagerController(ICommandBus commandBus)
+    {
+        _commandBus = commandBus;
+    }
+
+    [HttpPost]
+    public IActionResult Post(DefineCustomerCommand command)
+    {
+        _commandBus.Dispatch(command);
+        return Created("", command.Id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult Put(Guid id, ModifyCustomerCommand command)
+    {
+        _commandBus.Dispatch(command);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult Delete(Guid id)
+    {
+        var command = new RemoveCustomerCommand()
+        {
+            Id = id
+        };
+
+        _commandBus.Dispatch(command);
+        return NoContent();
+    }
 }
--- Mc2.CrudTest.Presentation/Server/Controllers/CustomerManagerQueryController.cs
+++ Mc2.CrudTest.Presentation/Server/Controllers/CustomerManagerQueryController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mc2.CrudTest.Presentation.Shared;
+using Mc2.Domain.Contracts.CustomerManagers;
+using Mc2.Query.Contracts.Requests;
+using Mc2.Query.Contracts.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mc2.CrudTest.Presentation.Server.Controllers;
 
@@ -6,4 +10,22 @@ namespace Mc2.CrudTest.Presentation.Server.Controllers;
 [Route("api/[controller]")]
 public class CustomerManagerQueryController : ControllerBase
 {
+    private readonly IQueryBus _queryBus;
+
+    public CustomerManagerQueryController(IQueryBus queryBus)
+    {
+        _queryBus = queryBus;
+    }
+
+    [HttpGet]
+    public IReadOnlyCollection<CustomerResponse> Get()
+    {
+        return _queryBus.Execute<GetAllCustomers, IReadOnlyCollection<CustomerResponse>>(new GetAllCustomers());
+    }
+
+    [HttpGet("{id:guid}")]
+    public CustomerResponse Get(Guid id)
+    {
+        return _queryBus.Execute<GetCustomerById, CustomerResponse>(new GetCustomerById(new CustomerId(id)));
+    }
 }
