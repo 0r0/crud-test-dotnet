dd6add65dae1914e9619836b915be651ea9fb422
mehdi <0r0.developer@gmail.com>
2024-01-10 22:58:56 +0330

    initiate new event handler and query handlers

--- Mc2.DBProjection/Handlers/CustomerEventHandlers.cs
+++ Mc2.DBProjection/Handlers/CustomerEventHandlers.cs
@@ -1,17 +1,31 @@
 using Mc2.CrudTest.Presentation.Shared;
 using Mc2.Domain.Contracts.CustomerManagers;
+using Neo4j.Driver;
 
 namespace Mc2.DBProjection.Handlers;
 
 public class CustomerEventHandlers :IEventHandler<CustomerDefined>,
-    IEventHandler<CustomerModified>
+    IEventHandler<CustomerModified>,
+    IEventHandler<CustomerRemoved>
 {
+    private readonly IAsyncTransaction _transaction;
+
+    public CustomerEventHandlers(IAsyncTransaction transaction)
+    {
+        _transaction = transaction;
+    }
+
     public void Handle(CustomerDefined @event)
     {
         throw new NotImplementedException();
     }
 
     public void Handle(CustomerModified @event)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Handle(CustomerRemoved @event)
     {
         throw new NotImplementedException();
     }
--- Mc2.Query/CustomerManagers/CustomerQueryHandlers.cs
+++ Mc2.Query/CustomerManagers/CustomerQueryHandlers.cs
@@ -5,7 +5,10 @@ using Mc2.Query.Contracts.Responses;
 namespace Mc2.Query.CustomerManagers;
 
 public class CustomerQueryHandlers : IQueryHandler<GetCustomerById,CustomerResponse>,
-    IQueryHandler<GetAllCustomers,IReadOnlyCollection<CustomerResponse>>
+    IQueryHandler<GetAllCustomers,IReadOnlyCollection<CustomerResponse>>,
+    IQueryHandler<IsSameEmailExist,bool>,
+    IQueryHandler<IsCustomerWithSameFirstNameLastNameAndBirthDateExist,bool
+    >
 {
     public CustomerResponse Handle(GetCustomerById query)
     {
@@ -13,6 +16,16 @@ public class CustomerQueryHandlers : IQueryHandler<GetCustomerById,CustomerRespo
     }
 
     public IReadOnlyCollection<CustomerResponse> Handle(GetAllCustomers query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Handle(IsSameEmailExist query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Handle(IsCustomerWithSameFirstNameLastNameAndBirthDateExist query)
     {
         throw new NotImplementedException();
     }
