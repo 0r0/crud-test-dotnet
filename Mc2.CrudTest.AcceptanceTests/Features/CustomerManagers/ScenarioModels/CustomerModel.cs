4e440f4ff8d36c8ec0a04e40b7ab919d2eebfed8
mehdi <0r0.developer@gmail.com>
2024-01-13 00:59:25 +0330

    move to another folder

---
+++ Mc2.CrudTest.AcceptanceTests/Features/CustomerManagers/ScenarioModels/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Mc2.CrudTest.AcceptanceTests.Features.CustomerManagers.ScenarioModels;
+
+public class CustomerModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public long PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string BankAccountNumber { get; set; }
+}
---
+++ Mc2.CrudTest.AcceptanceTests/Features/CustomerManagers/ScenarioModels/CustomerModelTransformer.cs
@@ -0,0 +1,40 @@
+﻿using Mc2.CrudTest.AcceptanceTests.ScreenPlay.CustomerManager.Commands;
+using TechTalk.SpecFlow.Assist;
+
+namespace Mc2.CrudTest.AcceptanceTests.Features.CustomerManagers.ScenarioModels;
+
+[Binding]
+public class CustomerModelTransformer
+{
+    [StepArgumentTransformation]
+    public DefineCustomerManagerCommand ConvertToDefineCommand(Table table)
+    {
+        var model = table.CreateInstance<CustomerModel>();
+        return new DefineCustomerManagerCommand()
+        {
+            BankAccountNumber = model.BankAccountNumber,
+            Email = model.Email,
+            Id = default,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            PhoneNumber = model.PhoneNumber,
+            DateOfBirth = model.DateOfBirth
+        };
+    }
+
+    [StepArgumentTransformation]
+    public ModifyCustomerManagerCommand ConvertToModifyCommand(Table table)
+    {
+        var model = table.CreateInstance<CustomerModel>();
+        return new ModifyCustomerManagerCommand()
+        {
+            BankAccountNumber = model.BankAccountNumber,
+            Email = model.Email,
+            Id = default,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            PhoneNumber = model.PhoneNumber,
+            DateOfBirth = model.DateOfBirth
+        };
+    }
+}
